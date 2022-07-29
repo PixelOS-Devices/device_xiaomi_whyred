5037c85c0b80934f41b4928712235c22ba4b8f82
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-07-29 12:56:58 +0600

    whyred: include couple of flags

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Flags
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
