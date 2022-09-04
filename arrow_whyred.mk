61f5fef15008777186479edbb1e62884a2512158
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-09-04 12:00:47 +0600

    whyred: arrowify

--- arrow_whyred.mk
+++ arrow_whyred.mk
@@ -9,21 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common aosp stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Flags
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_INCLUDE_WIFI_EXT := true
+# TARGET_SUPPORTS_QUICK_TAP := true
+# TARGET_INCLUDE_WIFI_EXT := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := arrow_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
