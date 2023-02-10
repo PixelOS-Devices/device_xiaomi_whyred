ac1075ea4d3c50f9f1db252d63f78749b7f17d82
Sajid Shahriar <63386478+sajidshahriar72543@users.noreply.github.com>
2023-02-10 22:47:32 +0600

    sdm660-common: Update kernel defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_SCREEN_DENSITY := 440
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/whyred
-TARGET_KERNEL_CONFIG := whyred-oldcam_defconfig
+TARGET_KERNEL_CONFIG := whyred_defconfig
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
