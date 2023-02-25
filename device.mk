e1b65b1b63e5bcccb79b1877dadd0e7d8048cd1c
sajidshahriar <sazidshahriar39@gmail.com>
2023-02-25 14:06:27 +0000

    whyred: commonize overlay dir

--- device.mk
+++ device.mk
@@ -40,8 +40,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-pe
+    $(DEVICE_PATH)/overlay
 
 # Powerhint
 PRODUCT_COPY_FILES += \
